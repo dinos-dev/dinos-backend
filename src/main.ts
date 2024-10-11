87d73b58b46c898a3de7dbb6333b0b9cdf95dc29
eomseung-yeol <co6747@naver.com>
2024-10-11 14:06:13 +0900

    Feat: 스웨거 api-created(201) 커스텀 리스폰스 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { initSwagger } from './core/swagger-config';
+import { initSwagger } from './core/swagger/swagger-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
