f23d78a8f519c147d3f4cc1b19852d7a59701213
eomseung-yeol <co6747@naver.com>
2024-10-26 14:40:50 +0900

    Feat: User 모델 설계 #11

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,10 +1,13 @@
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 export abstract class BaseModel {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,69 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { BaseModel } from 'src/common/entities/base.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseModel {
+  @PrimaryGeneratedColumn({
+    comment: 'PK',
+    type: 'integer',
+    unsigned: true,
+  })
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    length: 255,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  @Exclude({
+    toPlainOnly: true,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  phoneNumber: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+  })
+  firstName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+  })
+  lastName: string;
+
+  static signup({
+    email,
+    password,
+    phoneNumber,
+    firstName,
+    lastName,
+  }: {
+    email: string;
+    password: string;
+    phoneNumber: string;
+    firstName: string;
+    lastName: string;
+  }) {
+    const user = new User();
+    user.email = email;
+    user.password = password;
+    user.phoneNumber = phoneNumber;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    return user;
+  }
+}
