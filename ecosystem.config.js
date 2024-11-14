775a854888314be7acb759fc9cf152f7fbcc42b1
eomseung-yeol <co6747@naver.com>
2024-11-14 17:42:35 +0900

    Feat: PM2 Ecosystem 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dinos',
+      script: './dist/main.js',
+      instances: 0,
+      watch: false,
+      merge_logs: false,
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      // out_file: '/home/ubuntu/.pm2/logs/api-access.log',
+      // error_file: '/home/ubuntu/.pm2/logs/api-error.log',
+      autorestart: true,
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
