47035055e0720c1ae7c149d9edbf273bdd768ae7
eomseung-yeol <co6747@naver.com>
2024-11-14 17:56:33 +0900

    block test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,8 @@ module.exports = {
       watch: false,
       merge_logs: false,
       log_date_format: 'YYYY-MM-DD HH:mm Z',
-      // out_file: '/home/ubuntu/.pm2/logs/api-access.log', #
-      // error_file: '/home/ubuntu/.pm2/logs/api-error.log', #
+      // out_file: '/home/ubuntu/.pm2/logs/api-access.log',
+      // error_file: '/home/ubuntu/.pm2/logs/api-error.log',
       autorestart: true,
       exec_mode: 'cluster',
       env: {
