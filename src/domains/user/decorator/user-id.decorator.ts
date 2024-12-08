dddf542913405b5bf430a5802eede67c4f0cb4d7
eomseung-yeol <co6747@naver.com>
2024-12-08 13:52:56 +0900

    feat:  미들웨어(middleware)에서  express request 객체에서 추출한 user payload 의 sub 값만 추출하는 custom decorator 추가 #18

---
+++ src/domains/user/decorator/user-id.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+/**
+ * payLoad에서 User sub 값을 추출하는 데코레이터
+ */
+export const UserId = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  /**
+   * TODO:// payLoad의 user, sub가 middleware에서 추출되지 않았을 경우 어떻게 핸들링할지 정해야함.
+   * 현재는 null 값이 들어와도 넘기는 형식으로 처리
+   *
+   * */
+  return req?.user?.sub;
+});
