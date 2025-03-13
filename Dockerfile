90c71d848f4f8afcfef8170a423a26a538dd7531
eomseung-yeol <co6747@naver.com>
2025-03-13 22:18:23 +0900

    chore: remove csurf module

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,15 @@
-FROM node:20-alpine
+FROM node:alpine AS development
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+COPY pnpm-lock.yaml ./
 
 RUN npm i -g pnpm
 
-WORKDIR /app
-
-COPY package.json pnpm-lock.yaml ./
-
-RUN pnpm install
+RUN pnpm i
 
 COPY . .
 
-RUN pnpm build
-
-EXPOSE 3000
-
-CMD [ "pnpm", "start:prod" ]
+CMD ["pnpm", "start:dev"]
--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -1,8 +1,6 @@
 import { INestApplication } from '@nestjs/common';
-// import * as csurf from 'csurf';
 import * as helmet from 'helmet';
 import * as hpp from 'hpp';
-import * as compression from 'compression';
 
 /**
  * 공용 미들웨어 정의
@@ -10,16 +8,9 @@ import * as compression from 'compression';
 export function middleware(app: INestApplication): INestApplication {
   const isProduction = process.env.NODE_ENV === 'production';
 
-  app.use(compression());
-
   if (isProduction) {
-    // app.use(csurf());
-
     app.use(hpp());
-
-    app.use(helmet.hidePoweredBy());
-    app.use(helmet.xssFilter());
-    app.use(helmet.frameguard());
+    app.use(helmet);
   }
 
   return app;
