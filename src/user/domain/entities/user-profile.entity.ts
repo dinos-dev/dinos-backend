4a9dc96e8a532f2bfe2a666e3536cc3df20cab4d
eomseung-yeol <co6747@naver.com>
2025-08-22 15:50:25 +0900

    feat: add abstraction layer for Unit of Work pattern in transactions #41

--- src/user/domain/entities/user-profile.entity.ts
+++ src/user/domain/entities/user-profile.entity.ts
@@ -1,71 +1,71 @@
-import { BaseModel } from 'src/common/entities/base.entity';
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { User } from './user.entity';
+// import { BaseModel } from 'src/common/entities/base.entity';
+// import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+// import { User } from './user.entity';
 
-@Entity()
-export class UserProfile extends BaseModel {
-  @PrimaryGeneratedColumn({
-    comment: 'PK',
-    type: 'integer',
-    unsigned: true,
-  })
-  id: number;
+// @Entity()
+// export class UserProfile extends BaseModel {
+//   @PrimaryGeneratedColumn({
+//     comment: 'PK',
+//     type: 'integer',
+//     unsigned: true,
+//   })
+//   id: number;
 
-  @Column({
-    type: 'integer',
-    unsigned: true,
-    comment: '유저 id',
-  })
-  userId: number;
+//   @Column({
+//     type: 'integer',
+//     unsigned: true,
+//     comment: '유저 id',
+//   })
+//   userId: number;
 
-  // ------------------------------------------------------------------------ //
+//   // ------------------------------------------------------------------------ //
 
-  @Column({
-    length: 20,
-    comment: '프로필 닉네임',
-  })
-  nickName: string;
+//   @Column({
+//     length: 20,
+//     comment: '프로필 닉네임',
+//   })
+//   nickName: string;
 
-  @Column({
-    length: 50,
-    nullable: true,
-    comment: '프로필 소개 및 간단소개',
-  })
-  comment: string | null;
+//   @Column({
+//     length: 50,
+//     nullable: true,
+//     comment: '프로필 소개 및 간단소개',
+//   })
+//   comment: string | null;
 
-  @Column({
-    type: 'integer',
-    nullable: true,
-    comment: 'header front image id',
-  })
-  headerId: number | null;
+//   @Column({
+//     type: 'integer',
+//     nullable: true,
+//     comment: 'header front image id',
+//   })
+//   headerId: number | null;
 
-  @Column({
-    type: 'integer',
-    nullable: true,
-    comment: 'body front image id',
-  })
-  bodyId: number | null;
+//   @Column({
+//     type: 'integer',
+//     nullable: true,
+//     comment: 'body front image id',
+//   })
+//   bodyId: number | null;
 
-  @Column({
-    type: 'varchar',
-    length: 8,
-    nullable: true,
-    comment: 'avatar header color ( hex code )',
-  })
-  headerColor: string | null;
+//   @Column({
+//     type: 'varchar',
+//     length: 8,
+//     nullable: true,
+//     comment: 'avatar header color ( hex code )',
+//   })
+//   headerColor: string | null;
 
-  @Column({
-    type: 'varchar',
-    length: 8,
-    nullable: true,
-    comment: 'avatar body color ( hex code )',
-  })
-  bodyColor: string | null;
+//   @Column({
+//     type: 'varchar',
+//     length: 8,
+//     nullable: true,
+//     comment: 'avatar body color ( hex code )',
+//   })
+//   bodyColor: string | null;
 
-  // 1-to-1 ------------------------------------------------------------------- //
+//   // 1-to-1 ------------------------------------------------------------------- //
 
-  @OneToOne(() => User, (user) => user.id)
-  @JoinColumn({ name: 'user_id' })
-  user: User;
-}
+//   @OneToOne(() => User, (user) => user.id)
+//   @JoinColumn({ name: 'user_id' })
+//   user: User;
+// }
