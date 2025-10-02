b777cd92c1ba7e53277ab6690772d8609ebac67e
eomseung-yeol <co6747@naver.com>
2025-10-02 21:03:54 +0900

    fix: Handle HTTP response properly using custom interceptor #46

--- src/common/swagger/response/api-no-content-response.ts
+++ src/common/swagger/response/api-no-content-response.ts
@@ -1,6 +1,53 @@
+// import { applyDecorators, Type } from '@nestjs/common';
+// import { ApiExtraModels, ApiNoContentResponse, getSchemaPath } from '@nestjs/swagger';
+// import { NoContentResponseDto } from '../dto/no-content-response.dto';
+
+// /** 204(No Content) Response Template*/
+// export const ApiNoContentResponseTemplate = <DtoClass extends Type<unknown>>(params?: {
+//   description?: string;
+//   type?: DtoClass;
+//   isArray?: boolean;
+// }) => {
+//   if (params?.type) {
+//     const schema = {
+//       description: params.description,
+//       schema: {
+//         allOf: [
+//           // NoContentResponseDto 의 프로퍼티를 가져옴
+//           { $ref: getSchemaPath(NoContentResponseDto) },
+
+//           {
+//             properties: {
+//               result: params.isArray
+//                 ? {
+//                     type: 'array',
+//                     items: { $ref: getSchemaPath(params.type) },
+//                   }
+//                 : {
+//                     $ref: getSchemaPath(params.type),
+//                   },
+//             },
+//           },
+//         ],
+//       },
+//     };
+//     return applyDecorators(ApiExtraModels(NoContentResponseDto, params?.type), ApiNoContentResponse(schema));
+//   } else {
+//     const schema = {
+//       description: params?.description,
+//       schema: {
+//         allOf: [
+//           // ResponseDto 의 프로퍼티를 가져옴
+//           { $ref: getSchemaPath(NoContentResponseDto) },
+//         ],
+//       },
+//     };
+//     return applyDecorators(ApiExtraModels(NoContentResponseDto), ApiNoContentResponse(schema));
+//   }
+// };
+
 import { applyDecorators, Type } from '@nestjs/common';
-import { ApiExtraModels, ApiNoContentResponse, getSchemaPath } from '@nestjs/swagger';
-import { NoContentResponseDto } from '../dto/no-content-response.dto';
+import { ApiNoContentResponse } from '@nestjs/swagger';
 
 /** 204(No Content) Response Template*/
 export const ApiNoContentResponseTemplate = <DtoClass extends Type<unknown>>(params?: {
@@ -8,40 +55,11 @@ export const ApiNoContentResponseTemplate = <DtoClass extends Type<unknown>>(par
   type?: DtoClass;
   isArray?: boolean;
 }) => {
-  if (params?.type) {
-    const schema = {
-      description: params.description,
-      schema: {
-        allOf: [
-          // NoContentResponseDto 의 프로퍼티를 가져옴
-          { $ref: getSchemaPath(NoContentResponseDto) },
+  // 204 No Content는 응답 본문이 없으므로 스키마를 정의하지 않음
+  const schema = {
+    description: params?.description || 'No Content',
+    // schema를 정의하지 않아서 빈 응답으로 표시
+  };
 
-          {
-            properties: {
-              result: params.isArray
-                ? {
-                    type: 'array',
-                    items: { $ref: getSchemaPath(params.type) },
-                  }
-                : {
-                    $ref: getSchemaPath(params.type),
-                  },
-            },
-          },
-        ],
-      },
-    };
-    return applyDecorators(ApiExtraModels(NoContentResponseDto, params?.type), ApiNoContentResponse(schema));
-  } else {
-    const schema = {
-      description: params?.description,
-      schema: {
-        allOf: [
-          // ResponseDto 의 프로퍼티를 가져옴
-          { $ref: getSchemaPath(NoContentResponseDto) },
-        ],
-      },
-    };
-    return applyDecorators(ApiExtraModels(NoContentResponseDto), ApiNoContentResponse(schema));
-  }
+  return applyDecorators(ApiNoContentResponse(schema));
 };
