4380c264e9d8b7662f397f61c3a431ae0341b6d7
eomseung-yeol <co6747@naver.com>
2026-02-03 21:40:28 +0900

    feat: naver search api response 데이터 이슈로 인한 불필요한 테이블(Restaurants)의 필드값 제거 #63

---
+++ prisma/migrations/20260130123035_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `webview_url` on the `restaurants` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "restaurants" DROP COLUMN "webview_url";
