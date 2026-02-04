3266fd01e960200fd9740eb3539a317477acdc76
eomseung-yeol <co6747@naver.com>
2026-02-04 22:59:38 +0900

    feat: reviews 엔드포인트의 단일/다중 presignedURL 엔드포인트 추가 #65

--- src/review/application/review.service.ts
+++ src/review/application/review.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { FILE_UPLOAD_SERVICE } from 'src/common/config/common.const';
+import { CreatePresignedUrlDto } from 'src/common/dto/create.presigned-url.dto';
+import { PresignedUrlResponseDto } from 'src/common/dto/presigned-url.response.dto';
+import { IFileUploadService } from 'src/common/interface/file-upload.interface';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(
+    @Inject(FILE_UPLOAD_SERVICE)
+    private readonly fileUploadService: IFileUploadService,
+  ) {}
+
+  async getSignedUrl(dto: CreatePresignedUrlDto): Promise<PresignedUrlResponseDto> {
+    return await this.fileUploadService.createPresignedUrl(dto, 'reviews');
+  }
+}
